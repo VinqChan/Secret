b8ef32a79417679c945c171f91db21cb726478ff
Ethan Chen <ethan.chen@easiio.com>
2018-06-15 12:37:08 +0800

    add wechat share

--- ShareUmbrella/app/src/main/java/com/dangong/oksan/App.java
+++ ShareUmbrella/app/src/main/java/com/dangong/oksan/App.java
@@ -26,8 +26,8 @@ import okhttp3.OkHttpClient;
  */
 
 public class App extends Application{
-    private static final String APP_ID = "12312313212313213213";    //这个APP_ID就是注册APP的时候生成
-    private static final String APP_SECRET = "12312312313212313213213";
+    public static final String WECHAT_APP_ID = "12312313212313213213";    //这个APP_ID就是注册APP的时候生成
+    public static final String APP_SECRET = "12312312313212313213213";
     public IWXAPI api;      //这个对象是专门用来向微信发送数据的一个重要接口,使用强引用持有,所有的信息发送都是基于这个对象的
     @Override
     public void onCreate() {
@@ -56,7 +56,7 @@ public class App extends Application{
         registerWeChat(this);
     }
     public void registerWeChat(Context context) {   //向微信注册app
-        api = WXAPIFactory.createWXAPI(context, APP_ID, true);
-        api.registerApp(APP_ID);
+        api = WXAPIFactory.createWXAPI(context, WECHAT_APP_ID, true);
+        api.registerApp(WECHAT_APP_ID);
     }
 }
--- ShareUmbrella/app/src/main/java/com/dangong/oksan/activity/InvatationRewardActivity.java
+++ ShareUmbrella/app/src/main/java/com/dangong/oksan/activity/InvatationRewardActivity.java
@@ -2,13 +2,17 @@ package com.dangong.oksan.activity;
 
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.blankj.utilcode.util.ToastUtils;
 import com.dangong.oksan.R;
 import com.dangong.oksan.activity.base.BaseActivity;
+import com.dangong.oksan.wxapi.WechatShareManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +33,7 @@ public class InvatationRewardActivity extends BaseActivity {
     TextView withdrawalRuleTv;
     @BindView(R.id.own_info_iv)
     ImageView ownInfoIv;
+    private WechatShareManager mShareManager;
 
     @Override
     public int getLayoutId() {
@@ -46,14 +51,44 @@ public class InvatationRewardActivity extends BaseActivity {
         ButterKnife.bind(this);
         withdrawalRuleTv.setVisibility(View.VISIBLE);
         withdrawalRuleTv.setText(getString(R.string.reward_detail));
-
+        mShareManager = WechatShareManager.getInstance(this);
     }
 
-    @OnClick(R.id.copy_invation_code_tv)
-    public void onViewClicked() {
-        ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        String invationCode = invationCodeTv.getText().toString().substring(8);
-        cm.setText(invationCode);
-        ToastUtils.showShort("复制成功！");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.wchat_iv, R.id.link_iv,R.id.copy_invation_code_tv})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.wchat_iv:
+                if (!isWebchatAvaliable()) {
+                    ToastUtils.showShort( "请先安装微信");
+                    return;
+                }
+                WechatShareManager.ShareContentText mShareContentText = (WechatShareManager.ShareContentText) mShareManager.getShareContentText("微信文本分享");
+                mShareManager.shareByWebchat(mShareContentText, WechatShareManager.WECHAT_SHARE_TYPE_FRENDS);
+                break;
+            case R.id.link_iv:
+                break;
+            case R.id.copy_invation_code_tv:
+                ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                String invationCode = invationCodeTv.getText().toString().substring(8);
+                cm.setText(invationCode);
+                ToastUtils.showShort("复制成功！");
+                break;
+        }
+    }
+    private boolean isWebchatAvaliable() {
+        //检测手机上是否安装了微信
+        try {
+            getPackageManager().getPackageInfo("com.tencent.mm", PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
